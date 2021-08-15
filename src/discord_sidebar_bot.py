18f18d2bece0ad72d64d2c6454d58c8eb1220265
bout3fiddy <11488427+bout3fiddy@users.noreply.github.com>
2021-08-15 23:47:26 +0200

    init

---
+++ src/discord_sidebar_bot.py
@@ -0,0 +1,42 @@
+import asyncio
+from pycoingecko import CoinGeckoAPI
+
+import discord
+from discord.ext import commands
+
+
+bot = commands.Bot(command_prefix=".")
+token = "ODc2NTgxODQ2Mjc0Njc0NzM5.YRmKdg.nYVzjvbENl2oFfaovgcoJzyOKmw"
+guild_id = 850700789831958559
+
+COINGECKO = CoinGeckoAPI()
+
+
+async def send_update():
+
+    crv_price = COINGECKO.get_price(
+        ids="curve-dao-token", vs_currencies="usd"
+    )["curve-dao-token"]["usd"]
+    nickname = (
+        f"CRV ${crv_price}"
+    )
+    activity_status = "CRV Price"
+    await bot.get_guild(guild_id).me.edit(nick=nickname)
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching, name=activity_status
+        )
+    )
+    await asyncio.sleep(60)  # in seconds
+
+
+@bot.event
+async def on_ready():
+    """
+    When discord client is ready
+    """
+    while True:
+        await send_update()
+
+
+bot.run(token)
